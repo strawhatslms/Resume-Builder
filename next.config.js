7b3b8e26cf2e6e49dfbbbc4884986f81f488c30a
strawhats <strawhatslms@gmail.com>
2025-10-18 00:29:25 +0530

    Commit 2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
+  webpack: (config, { isServer }) => {
     config.resolve.alias.canvas = false;
+    if (isServer) {
+      config.externals = [...config.externals, 'canvas'];
+    }
     return config;
   },
 }
